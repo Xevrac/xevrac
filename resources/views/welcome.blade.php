04521a6c9c734c8fdfacf272df24e2dc8b2fb289
Xevrac <crypto003@gmail.com>
2021-12-19 00:09:16 +1100

    removed spacer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -97,8 +97,6 @@
                                 GitHub
                             </a>
 
-                            <div class="border-gray-500 md:border-t-0 md:border-l ml-5">&nbsp;</div>
-
                             <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="ml-4 w-5 h-5 text-red-400 dark:text-white">
                             <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
 
